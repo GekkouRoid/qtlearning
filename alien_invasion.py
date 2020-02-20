ae14eecd89060b8d58b3a1096448069469c31d30
asahi <gekkou.hm@gmail.com>
2020-02-20 21:11:14 +0800

    Add a ship.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,12 +1,16 @@
 import sys
 import pygame
+from settings import Settings
 
 
 def run_game():
     # initiation
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    # bg_color = (240, 240, 240)
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+    screen.fill(ai_settings.bg_color)
 
     while True:
         # monitor keyboard & mouse event
@@ -15,7 +19,9 @@ def run_game():
                 sys.exit()
 
         # show the screen
+        # screen.fill(bg_color)
         pygame.display.flip()
+
 
 run_game()
 
---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings():
+    """ Store all the settings of the game."""
+
+    def __init__(self):
+        """initialize the setting of the game."""
+        # screen setting
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+
